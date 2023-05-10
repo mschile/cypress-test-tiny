e327e2f265fee84ec5504a2d133999d76e6acde3
Matt Schile <mschile@cypress.io>
2023-05-09 22:03:38 -0600

    updates

--- cypress/support/server.js
+++ cypress/support/server.js
@@ -2,8 +2,10 @@ const https = require('http');
 
 setTimeout(() => {
   https.createServer({}, (req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('Hello World!');
-    res.end();
-  }).listen(8080);
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    res.write('Hello World!')
+    res.end()
+  }).listen(8080, () => {
+    console.log('Server running at http://localhost:8080/')
+  })
 }, 5000)

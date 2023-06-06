5f544079ba1d2177c21446da30b2250608dafcb2
Matt Schile <mschile@cypress.io>
2023-06-06 12:11:37 -0600

    updating origin test

--- cypress/e2e/2-advanced-examples/origin.cy.js
+++ cypress/e2e/2-advanced-examples/origin.cy.js
@@ -8,9 +8,10 @@ context('Cross Origin', () => {
   it('visits a cross origin page', () => {
     // Alias the route to wait for its response
     cy.intercept('GET', '**/comments/*').as('getComment')
-    cy.visit('https://example.cypress.io/commands/aliasing')
-
+    
     cy.origin('https://example.cypress.io', () => {
+      cy.visit('https://example.cypress.io/commands/aliasing')
+      cy.viewport(320, 480)
       // we have code that gets a comment when
       // the button is clicked in scripts.js
       cy.get('.network-btn').click()

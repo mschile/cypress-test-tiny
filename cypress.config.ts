7716886df225ef8937deb281dd76ba4ab278e0f5
Matt Schile <mschile@cypress.io>
2023-07-11 12:42:39 -0600

    adding traversals spec

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,10 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   projectId: '9rttcd',
+  hosts: {
+    'foobar.com': '127.0.0.1',
+  },
+  port: 1234,
   e2e: {
     experimentalMemoryManagement: true,
     video: false,

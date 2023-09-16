a0e25be8312fb4d91eedfe1270f767156e7005d8
Matthew Schile <mschile@cypress.io>
2023-09-15 21:03:43 -0600

    updates

---
+++ cypress/e2e/data-urls.cy.js
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+describe('data urls', () => {
+  beforeEach(() => {
+    cy.visit('cypress/fixtures/dom-long-data-urls.html')
+  })
+
+  it('loads data urls', () => {
+    
+  })
+})
